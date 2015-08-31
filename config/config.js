1446099009ede9ad157db5be4e61a9253fdccaa0
happykiller <fabrice.rosito@gmail.com>
2015-08-31 13:57:48 +0200

    update

--- config/config.js
+++
@@ -1,4 +0,0 @@
-$.Oda.Context.host = "http://localhost/";
-$.Oda.Context.rest = "http://localhost/ODA-POK/server/";
-$.Oda.Context.projectLabel = "Oda Pok";
-$.Oda.Context.resources = "http://localhost/ODA-POK/server/resources/";
