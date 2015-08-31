9391edfe2b648f4f00d7700c696c323bcc7915eb
happykiller <fabrice.rosito@gmail.com>
2015-08-31 13:53:34 +0200

    update

--- bower_components/Oda/README.md
+++
@@ -1,2 +0,0 @@
-# ODA_FW_CLIENT
-Client for the full framework Oda
--- bower_components/Oda/deploy/app/config/config.js
+++
@@ -1,4 +0,0 @@
-$.Oda.Context.host = "http://localhost/";
-$.Oda.Context.rest = "http://localhost/server/";
-$.Oda.Context.projectLabel = "myApp";
-$.Oda.Context.resources = "http://localhost/server/resources/";
