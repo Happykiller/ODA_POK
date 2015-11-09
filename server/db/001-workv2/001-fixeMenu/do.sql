3f0327ac727511af0c0ef5b5917a16b82e0d7f50
Rosito Fabrice <fabrice.rosito@gmail.com>
2015-11-09 22:33:26 +0100

    update

---
+++ server/db/001-workv2/001-fixeMenu/do.sql
@@ -0,0 +1,11 @@
+UPDATE `@prefix@api_tab_menu`
+SET `Lien` = 'api_page_stats.html'
+WHERE `pok_api_tab_menu`.`id` = 4;
+
+UPDATE `@prefix@api_tab_menu`
+SET `Lien` = 'api_page_admin.html'
+WHERE `pok_api_tab_menu`.`id` = 5;
+
+UPDATE `@prefix@api_tab_menu`
+SET `Lien` = 'api_page_supervision.html'
+WHERE `pok_api_tab_menu`.`id` = 6;
